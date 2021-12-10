eaad745d47a332c124d972160b09fb2d13c276a9
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-10 10:53:29 -0800

    added to project_resources table and seed

--- data/seeds/01_make_projects.js
+++ data/seeds/01_make_projects.js
@@ -12,9 +12,9 @@ const tasks = [
   { task_description: 'finish endpoints', task_notes: 'start with GETs', project_id: 1 }
 ]
 
-const project_resources = {
-
-}
+const project_resources = [
+  { project_id: 1, resource_id: 1 }
+]
 
 exports.seed = async function(knex) {
   await knex('projects').insert(projects)
