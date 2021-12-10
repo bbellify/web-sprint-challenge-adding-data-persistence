44f165313af5057ae028279d4d38addc34f664c9
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-10 09:11:18 -0800

    server up, dependencies added

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,13 @@
 // build your `/api/projects` router here
+const express = require('express')
+
+const router = express.Router()
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express')
+const router = require('./project/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api', router)
+
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
+module.exports = server
