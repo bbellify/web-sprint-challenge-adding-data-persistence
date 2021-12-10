5b8ddfc327e5bb00458a90daac0dab4235bbf98b
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-10 11:15:57 -0800

    MVP met and cleanup

--- api/task/model.js
+++ api/task/model.js
@@ -11,7 +11,14 @@ async function getTasks() {
     //     on p.project_id = t.project_id;
     
         const rows = await db('tasks as t')
-            .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_name', 'p.project_description')
+            .select(
+                't.task_id', 
+                't.task_description', 
+                't.task_notes', 
+                't.task_completed', 
+                'p.project_name', 
+                'p.project_description'
+                )
             .leftJoin('projects as p', 'p.project_id', 't.project_id')
     
         const response = []
